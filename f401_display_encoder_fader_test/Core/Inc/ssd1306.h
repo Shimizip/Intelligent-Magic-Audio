c29fd0d7b33c4a8045300078f16688b49fbd49b9
sbanasia <szymon_jozef.banasiak@smail.th-koeln.com>
2024-06-13 15:05:19 +0200

    Old f401_display_encoder_fader_test

--- f401_display_encoder_fader_test/Core/Inc/ssd1306.h
+++ f401_display_encoder_fader_test/Core/Inc/ssd1306.h
@@ -30,7 +30,7 @@
 
 // SSD1306 LCD height in pixels
 #ifndef SSD1306_HEIGHT
-#define SSD1306_HEIGHT          64
+#define SSD1306_HEIGHT          128
 #endif // SSD1306_HEIGHT
 
 #ifndef SSD1306_COM_LR_REMAP
